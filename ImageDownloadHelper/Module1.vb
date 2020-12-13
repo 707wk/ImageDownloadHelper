568e92b6056c370d42dd6c132985376caec04a9c
汪恳 <1174843091@qq.com>
2020-12-13 09:34:53 +0800

    1.添加启动参数 2.修改下载错误时的处理方式

--- ImageDownloadHelper/Module1.vb
+++ ImageDownloadHelper/Module1.vb
@@ -1,11 +1,10 @@
 ﻿Imports System.Net
-Imports System.Web
 
 Module Module1
 
     Sub Main(args As String())
 
-        Console.WriteLine(String.Join("#", args))
+        Console.WriteLine(System.Reflection.Assembly.GetExecutingAssembly().Location)
 
         ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 Or
             SecurityProtocolType.Tls Or
@@ -14,30 +13,33 @@ Module Module1
 
         Dim assemblyLocation = System.Reflection.Assembly.GetExecutingAssembly().Location
         Console.WriteLine($"程序版本 :{System.Diagnostics.FileVersionInfo.GetVersionInfo(assemblyLocation).ProductVersion}")
+        Dim pageUrl As String
+        If args.Count = 1 Then
+            pageUrl = args(0).Substring(args(0).IndexOf(":") + 1)
+            Console.Write($"下载页面地址 :{pageUrl}")
+        Else
+            Console.Write($"输入下载页面地址 :")
+            pageUrl = Console.ReadLine
+        End If
 
-        Console.Write($"输入下载页面地址 :")
-        Dim pageUrl As String = Console.ReadLine
+        Console.WriteLine($"开始下载 :{Now}")
+        Try
+            Dim tmp = _WebsiteFactory.Create(pageUrl)
+            tmp.DirectoryPath = "D:\Downloads"
+            tmp.Download()
 
-        Do
-            Console.WriteLine($"开始下载 :{Now}")
-            Try
-                Dim tmp = _WebsiteFactory.Create(pageUrl)
-                tmp.DirectoryPath = "D:\Downloads"
-                tmp.Download()
+            Exit Sub
 
-                Exit Sub
-
-            Catch ex As Exception
-                Console.WriteLine($"下载异常 :{ex.Message}")
-                If MsgBox($"下载异常 {ex.Message},是否尝试继续下载?",
-                          MsgBoxStyle.YesNo,
-                          pageUrl) <> MsgBoxResult.Yes Then
-                    Exit Sub
-                End If
-
-            End Try
-        Loop
-
+        Catch ex As Exception
+            Console.WriteLine($"下载异常 :{ex.Message}")
+            If MsgBox($"下载异常 {ex.Message},是否尝试继续下载?",
+                      MsgBoxStyle.YesNo,
+                      pageUrl) <> MsgBoxResult.Yes Then
+            Else
+                Process.Start(System.Reflection.Assembly.GetExecutingAssembly().Location, args(0))
+            End If
+            Exit Sub
+        End Try
 
     End Sub
 
